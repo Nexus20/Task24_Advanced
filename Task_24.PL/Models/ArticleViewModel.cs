6598e7c81783a537fefdcc6125704c8686e96feb
Voidwalker20 <chubarov.zheka@gmail.com>
2021-08-10 12:59:05 +0300

    Added annnotations to models

--- Task_24.PL/Models/ArticleViewModel.cs
+++ Task_24.PL/Models/ArticleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Task_24.PL.Models {
         public string Title { get; set; }
         public string ImagePath { get; set; }
         public string Text { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
         public DateTime PublicationDate { get; set; }
     }
 }
--- Task_24.PL/Models/CommentViewModel.cs
+++ Task_24.PL/Models/CommentViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Task_24.PL.Models {
     public class CommentViewModel : BaseViewModel {
+        [Required]
+        [StringLength(32, MinimumLength = 3)]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(300, MinimumLength = 1)]
         public string Text { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
         public DateTime PublicationDate { get; set; }
 
         public CommentViewModel() {
